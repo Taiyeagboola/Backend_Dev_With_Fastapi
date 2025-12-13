e6c245cc9c85c8431a82a828b2e25e9d8d1e1f90
Taiye Agboola Oluwasegun <taiyeagboola2003@gmail.com>
2025-12-13 23:44:04 +0100

    Did some testing

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,13 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
+import pytest
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.database import get_db
 from app.main import app
 from app import schemas
 from app.config import settings
+from app.database import Base
 
 # SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:taiye1692@localhost:5432/fastapi_test"
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
@@ -13,8 +15,8 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
 
-Base = declarative_base()
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -26,15 +28,38 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 
-client = TestClient(app)
 
-def test_root():
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    
+    
+
+
+def test_root(client):
     res = client.get("/")
     print(res.json().get('message'))
     assert res.json().get('message') == 'Hello World!'
     assert res.status_code == 200
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users", json={"email": "taiye1@gmail.com", "password": "taiye12"})
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == 'taiye1@gmail.com'
